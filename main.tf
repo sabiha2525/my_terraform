0ccb88fa39b853f8152a957ccb5869cf88ceb3b6
sabiha2525 <109275842+sabiha2525@users.noreply.github.com>
2022-09-19 18:53:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,25 @@
-resource "google_project" "my_project" {
-  name       = "My Project"
-  project_id = "252830"
+resource "google_app_engine_standard_app_version" "myapp_v1" {
+  version_id = "v1"
+  service    = "myapp"
+  runtime    = "nodejs14"
+
+  entrypoint {
+    shell = "node ./app.js"
+  }
+
+  deployment {
+    zip {
+      source_url = "https://storage.googleapis.com/${var.bucket}/${google_storage_bucket_object.appengineae.name}"
+    }
+  }
+
+  env_variables = {
+    port = "${var.port}"
+  }
 }
 
+resource "google_storage_bucket_object" "appengineae" {
+  name   = "appengineae"
+  bucket = "${var.bucket}"
+  source = "./appengine.zip"
+}
